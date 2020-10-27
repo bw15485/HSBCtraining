837f70b050a19e5c333e40b72883269d18c23ddf
ben <bw15485@my.bristol.ac.uk>
2020-10-27 21:33:39 +0000

    Add constructor class

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+public class Constructor {
+
+	public static void main(String[] args) {
+		Manchester ref = new Manchester();
+	}
+
+}
+
+class Manchester {
+	public Manchester() {
+		System.out.println("this is a constructor");
+	}
+}
